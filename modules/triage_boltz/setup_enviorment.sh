10fdffd47979de2507eb9cc0f0a6de4fe255faea
jilimcaoco <limcaoco@umich.edu>
2025-10-06 09:26:19 -0400

    feat: update SLURM script to use environment variables for email and account, and remove unnecessary job script files

--- modules/triage_boltz/setup_enviorment.sh
+++ modules/triage_boltz/setup_enviorment.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set the environment variable PROJECT_DIR
-export PROJECT_DIR="/home/limcaoco/turbo/limcaoco/boltz_benchmark"
-export PYTHONPATH="/home/limcaoco/turbo/limcaoco/boltz_benchmark/modules:$PYTHONPATH"
-
+export PROJECT_DIR="/PATH/TO/PROJECT"  # Replace with your actual project path
+export PYTHONPATH="/PATH/TO/MODULES/modules:$PYTHONPATH"
+export SLURM_ACCOUNT="SLURM_ACCOUNT_NAME"  # Replace with your actual SLURM account name
 # Set the environment variable for SLURM email notifications
-export SLURM_EMAIL="limcaoco@umich.edu"
+export SLURM_EMAIL="SLURM_EMAIL_ADDRESS"  # Replace with your actual SLURM email address
