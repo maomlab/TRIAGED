bc4e4f6940f66b3234cf10daa34f228249eacb19
jilimcaoco <limcaoco@umich.edu>
2025-10-08 12:05:56 -0400

    chore: remove unnecessary comments from __init__.py to clean up the code

--- modules/triage_boltz/__init__.py
+++ modules/triage_boltz/__init__.py
@@ -1,10 +1,1 @@
-# This is a generic __init__.py script
-# It marks the directory as a Python package.
 
-# You can use this file to initialize the package, import modules, or define package-level variables.
-
-# Example: Importing a module within the package
-# from .module_name import some_function
-
-# Example: Defining a package-level variable
-# __version__ = "1.0.0"
